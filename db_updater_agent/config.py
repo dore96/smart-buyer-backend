7dccfcc29679824228478ffb96a333d40107e35b
Dor Edelman <doredelman@gmail.com>
2023-09-16 01:26:02 +0300

    extand configuration

--- db_updater_agent/config.py
+++ db_updater_agent/config.py
@@ -1,13 +1,1 @@
-# PostgreSQL connection settings
-db_settings = {
-    "host": "ec2-18-202-145-226.eu-west-1.compute.amazonaws.com",
-    "database": "d5b2b15ehbtied",
-    "user": "jdodhzqgmloamf",
-    "password": "1b44eaa8c8cb8ef499bd946d38345271d2d21200d2de37b6272aaffd0db0e0d8",
-    "port": "5432"
-}
-
-# Paths to web scraping scripts, XML reader script, and XML data folder
-web_scrapers_scripts_path = r'C:\Users\dored\Desktop\SmartBuyer-Backend\smart-buyer-backend\db_updater_agent\scripts\web_scrapers'
-xml_reader_script = r'C:\Users\dored\Desktop\SmartBuyer-Backend\smart-buyer-backend\db_updater_agent\scripts\xml_multiple_reader.py'
-xml_data_folder_path = r'C:\Users\dored\Desktop\SmartBuyer-Backend\xml_data'
+# secrets configuration
--- src-Backend-API/app/config.py
+++ src-Backend-API/app/config.py
@@ -1,4 +1,1 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://jdodhzqgmloamf:1b44eaa8c8cb8ef499bd946d38345271d2d21200d2de37b6272aaffd0db0e0d8@ec2-18-202-145-226.eu-west-1.compute.amazonaws.com:5432/d5b2b15ehbtied'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "dorIsTheKingdrtfsgyuhdji"
+#secrets configuration
